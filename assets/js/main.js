7a8847f3f3530a703cd786a866c68f8962ab9781
Luis <hhavocs@yahoo.com>
2018-09-01 14:32:57 -0400

    working with timers

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,20 @@
 
-var $startBtn =$("#start"); 
+var $startBtn = $("#start");
 var $timer = $('#timer');
-var $question =$('#question');
+var $question = $('#question');
 var $answers = $('.answer');
 var $trivia = $('#trivia');
+var $rAnswer;
 
 
 var gameRunning = false;
+var ansRunning = false;
 var questionSelected = 0;
 var questionTimer;
+var ansTimer;
 
 
-var questions = [
-    { q : "Question", a:"Answer 1", b:"Answer 2", c:"Answer 3", d:"Answer 4", correct:"a1"}
-];
+
 
 $startBtn.click(startSlideshow);
 $answers.click(checkAnswer);
@@ -24,49 +25,122 @@ function startSlideshow() {
 
     // TODO: Use showImage to hold the setInterval to run nextImage.
     console.log('IN');
-    if(!gameRunning){
+
+    if (!gameRunning) {
+        $trivia.addClass("trivia");
         $(this).hide();
-        questionTimer = setInterval(modTimer, 1000);
         gameRunning = true;
+        unsortTrivia(questions);
         nextQuestion(questionSelected);
     }
-};  
+};
 
-function modTimer(){
+function modTimer() {
     var time = parseInt($timer.text());
     time--;
     $timer.text(parseInt(time));
-};
-
-
-function nextQuestion(num){
-    $question.text(questions[num].q);
-    $("#a").text(questions[num].a);
-    $("#a").attr("value", "a4");
-    $("#b").text(questions[num].b);
-    $("#b").attr("value", "a1");
-    $("#c").text(questions[num].c);
-    $("#c").attr("value", "a2");
-    $("#d").text(questions[num].d);
-    $("#d").attr("value", "a3");
-};
-
-function checkAnswer(){
-    alert($(this).attr("value"));
-    var ans = $(this).attr("value"); 
-    if(ans === questions[questionSelected].correct){
-        alert(">>>>>>>>>>>>>");
+    console.log(time);
+    counter(time, 30);
+    if (time < 0) {
         showAnswer();
     }
-}
+};
+function modAnsTimer() {
+    var time = parseInt($timer.text());
+    time--;
+    $timer.text(parseInt(time));
+    counter(time, 15);
+    ansRunning = true;
+    console.log(time);
+    if (time < 0) {
+        ansRunning = false;
+        questionSelected++;
+        $rAnswer.removeClass("ranswer");
+        $answers.removeClass("wanswer");
+        nextQuestion(questionSelected);
+    }
 
-function showAnswer(){
-    clearInterval(questionTimer);
-    $trivia.hide();
+};
+
+function nextQuestion(num) {
+
+    console.log(questions[num]);
+
+
     $timer.text(30);
-    questionTimer = setInterval(modTimer, 1000);
-    var $selected = $('#'+questions[questionSelected].correct);
-    console.log($selected);
+    clearInterval(ansTimer);
+        
 
+    if(questionSelected<10){
+        var rAnswer = questions[num].correct.substr(1, questions[num].correct.length); 
+        rAnswer = questions[num].a[rAnswer-1];
+        unsortTrivia(questions[num].a);
+        console.log(questions[num]);
+        
+        if(questions[num].a[0] == rAnswer){
+            questions[num].correct = "a1";
+        }else if(questions[num].a[1] == rAnswer){
+            questions[num].correct = "a2";
+        }if(questions[num].a[2] == rAnswer){
+            questions[num].correct = "a3";
+        }else if(questions[num].a[3] == rAnswer){
+            questions[num].correct = "a4";
+        }
+        $rAnswer = $("#"+questions[num].correct);
+    
+        for(var i=0; i<questions[num].a.length; i++){
+            $("#a"+(i+1)).text(questions[num].a[i]);
+        }
+    
+        questionTimer = setInterval(modTimer, 1000);
+        $question.text(questions[num].q);
+    }
+
+};
+
+function checkAnswer() {
+    alert($(this).attr("id"));
+    var ans = $(this).attr("id");
+    if(!ansRunning){
+        if (ans === questions[questionSelected].correct && questionTimer > 0) {
+            alert(">>>>>>>>>>>>>");
+            showAnswer();
+            
+        }else{
+            $(this).addClass("wanswer").delay( 800 );
+            showAnswer();
+        }
+    }
+    
 }
 
+function showAnswer() {
+    clearInterval(questionTimer);
+    $timer.text(15);
+    ansTimer = setInterval(modAnsTimer, 1000);
+    $rAnswer.addClass("ranswer");
+};
+
+
+function unsortTrivia(array) {
+
+    console.log(array);
+
+
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    console.log(array);
+
+}
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,3 +1,96 @@
-var questionSelected = [
-    
-]
+var questions = [
+    {
+        q: "Question",
+        a: ["Answer 1", "CORRECT", "Answer 3", "Answer 4"],
+        correct: "a2"
+    },
+    {
+        q: "Question2",
+        a: ["CORRECT", "Answer 2", "Answer 3", "Answer 4"],
+        correct: "a1"
+    },
+    {
+        q: "Question3",
+        a: ["Answer3 1", "Answer 2", "CORRECT", "Answer 4"],
+        correct: "a3"
+    },
+    {
+        q: "Question4",
+        a: ["Answer4 1", "Answer 2", "Answer 3", "CORRECT"],
+        correct: "a4"
+    },
+    {
+        q: "Question5",
+        a: ["Answer5 1", "CORRECT", "Answer 3", "Answer 4"],
+        correct: "a2"
+    },
+    {
+        q: "Question6",
+        a: ["CORRECT", "Answer 2", "Answer 3", "Answer 4"],
+        correct: "a1"
+    },
+    { 
+        q: "Question7", 
+        a: ["Answer7 1", "Answer 2", "Answer 3", "CORRECT"], 
+        correct: "a4" 
+    },
+    { 
+        q: "Question8", 
+        a: ["Answer8 1", "Answer 2", "CORRECT", "Answer 4"], 
+        correct: "a3" 
+    },
+    { 
+        q: "Question9", 
+        a: ["CORRECT", "Answer 2", "Answer 3", "Answer 4"], 
+        correct: "a1" 
+    },
+    { 
+        q: "Question10", 
+        a: ["Answer0 1", "CORRECT", "Answer 3", "Answer 4"], 
+        correct: "a2" 
+    },
+    { 
+        q: "Question11", 
+        a: ["Answer1 1", "CORRECT", "Answer 3", "Answer 4"], 
+        correct: "a2" 
+    },
+    { 
+        q: "Question12", 
+        a: ["Answer2 1", "Answer 2", "Answer 3", "CORRECT"], 
+        correct: "a4" 
+    },
+    { 
+        q: "Question13", 
+        a: ["CORRECT", "Answer 2", "Answer 3", "Answer 4"], 
+        correct: "a1" 
+    },
+    {
+        q: "What does the acronym USB stand for when referring to a computer port?",
+        a: ["Answer42 1", "Answer2 2", "CORRECT", "Answer2 4"],
+        correct: "a3"
+    }
+];
+
+
+
+
+function counter(val, maxTime) {
+    var $circle = $('#svg #bar');
+
+    if (isNaN(val)) {
+        val = 30;
+    }
+    else {
+        var r = $circle.attr('r');
+        var c = Math.PI * (r * 2);
+
+        if (val < 0) { val = 0; }
+        if (val > maxTime) { val = maxTime; }
+
+        var pct = ((maxTime - val) / maxTime) * c;
+
+        $circle.css({ strokeDashoffset: pct });
+
+        $('#cont').attr('data-pct', val);
+    }
+};
